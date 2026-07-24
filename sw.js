ab6b391c5bc960476c7e6af59cb6d13f5f65ff1c
tndudbrad-ux <tndud.brad@gmail.com>
2026-07-24 14:36:21 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='hwon-v20';
+var CACHE='hwon-v21';
 var CORE=['/','/style.css','/hwon-bot.js','/hwon-ui.js','/hwon-ui.css','/icon-192.png'];
 self.addEventListener('install',function(e){
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(CORE);}).then(function(){return self.skipWaiting();}));
